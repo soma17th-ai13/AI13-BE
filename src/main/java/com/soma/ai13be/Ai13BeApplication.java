c775fb19cf6c79319736c2a12d39e77a2c9d5d22
minbros <realminbros@gmail.com>
2026-05-05 16:03:25 +0900

    Initial Commit

---
+++ src/main/java/com/soma/ai13be/Ai13BeApplication.java
@@ -0,0 +1,13 @@
+package com.soma.ai13be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ai13BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ai13BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soma/ai13be/Ai13BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soma.ai13be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ai13BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
